019c8135395f92533eee4ae2c7df8f0675d2c37c
Sheehan Olver <solver@mac.com>
2019-11-19 10:42:23 +0000

    Update ldiv.jl

--- src/ldiv.jl
+++ src/ldiv.jl
@@ -5,45 +5,45 @@ for Typ in (:Ldiv, :Rdiv)
             B::BType
         end
 
-        $Typ{StyleA, StyleB}(A::AType, B::BType) where {StyleA,StyleB,AType,BType} =
+        @inline $Typ{StyleA, StyleB}(A::AType, B::BType) where {StyleA,StyleB,AType,BType} =
             $Typ{StyleA,StyleB,AType,BType}(A,B)
 
-        $Typ(A::AType, B::BType) where {AType,BType} =
+        @inline $Typ(A::AType, B::BType) where {AType,BType} =
             $Typ{typeof(MemoryLayout(AType)),typeof(MemoryLayout(BType)),AType,BType}(A, B)
 
-        BroadcastStyle(::Type{<:$Typ}) = ApplyBroadcastStyle()
-        broadcastable(M::$Typ) = M
+        @inline BroadcastStyle(::Type{<:$Typ}) = ApplyBroadcastStyle()
+        @inline broadcastable(M::$Typ) = M
 
         similar(A::$Typ, ::Type{T}, axes) where T = similar(Array{T}, axes)
         similar(A::$Typ, ::Type{T}) where T = similar(A, T, axes(A))
         similar(A::$Typ) = similar(A, eltype(A))
 
-        copy(M::$Typ) = copyto!(similar(M), M)
-        materialize(M::$Typ) = copy(instantiate(M))
+        @inline copy(M::$Typ) = copyto!(similar(M), M)
+        @inline materialize(M::$Typ) = copy(instantiate(M))
     end
 end
 
-size(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractMatrix}) = (size(L.A, 2),size(L.B,2))
-size(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractVector}) = (size(L.A, 2),)
-axes(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractMatrix}) = (axes(L.A, 2),axes(L.B,2))
-axes(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractVector}) = (axes(L.A, 2),)
-length(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractVector}) =size(L.A, 2)
+@inline size(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractMatrix}) = (size(L.A, 2),size(L.B,2))
+@inline size(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractVector}) = (size(L.A, 2),)
+@inline axes(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractMatrix}) = (axes(L.A, 2),axes(L.B,2))
+@inline axes(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractVector}) = (axes(L.A, 2),)
+@inline length(L::Ldiv{<:Any,<:Any,<:Any,<:AbstractVector}) =size(L.A, 2)
 
-size(L::Rdiv) = (size(L.A, 1),size(L.B,1))
-axes(L::Rdiv) = (axes(L.A, 1),axes(L.B,1))
+@inline size(L::Rdiv) = (size(L.A, 1),size(L.B,1))
+@inline axes(L::Rdiv) = (axes(L.A, 1),axes(L.B,1))
 
-_ldivaxes(::Tuple{}, ::Tuple{}) = ()
-_ldivaxes(::Tuple{}, Bax::Tuple) = Bax
-_ldivaxes(::Tuple{<:Any}, ::Tuple{<:Any}) = ()
-_ldivaxes(::Tuple{<:Any}, Bax::Tuple{<:Any,<:Any}) = (OneTo(1),last(Bax))
-_ldivaxes(Aax::Tuple{<:Any,<:Any}, ::Tuple{<:Any}) = (last(Aax),)
-_ldivaxes(Aax::Tuple{<:Any,<:Any}, Bax::Tuple{<:Any,<:Any}) = (last(Aax),last(Bax))
+@inline _ldivaxes(::Tuple{}, ::Tuple{}) = ()
+@inline _ldivaxes(::Tuple{}, Bax::Tuple) = Bax
+@inline _ldivaxes(::Tuple{<:Any}, ::Tuple{<:Any}) = ()
+@inline _ldivaxes(::Tuple{<:Any}, Bax::Tuple{<:Any,<:Any}) = (OneTo(1),last(Bax))
+@inline _ldivaxes(Aax::Tuple{<:Any,<:Any}, ::Tuple{<:Any}) = (last(Aax),)
+@inline _ldivaxes(Aax::Tuple{<:Any,<:Any}, Bax::Tuple{<:Any,<:Any}) = (last(Aax),last(Bax))
 
 @inline ldivaxes(A, B) = _ldivaxes(axes(A), axes(B))
 
-ndims(L::Ldiv) = ndims(last(L.args))
-eltype(M::Ldiv) = promote_type(Base.promote_op(inv, eltype(M.A)), eltype(M.B))
-eltype(M::Rdiv) = promote_type(eltype(M.A), Base.promote_op(inv, eltype(M.B)))
+@inline ndims(L::Ldiv) = ndims(last(L.args))
+@inline eltype(M::Ldiv) = promote_type(Base.promote_op(inv, eltype(M.A)), eltype(M.B))
+@inline eltype(M::Rdiv) = promote_type(eltype(M.A), Base.promote_op(inv, eltype(M.B)))
 
 
 check_ldiv_axes(A, B) =
@@ -54,34 +54,34 @@ check_rdiv_axes(A, B) =
 
 
 
-function instantiate(L::Ldiv)
+@inline function instantiate(L::Ldiv)
     check_ldiv_axes(L.A, L.B)
     Ldiv(instantiate(L.A), instantiate(L.B))
 end
 
-function instantiate(L::Rdiv)
+@inline function instantiate(L::Rdiv)
     check_rdiv_axes(L.A, L.B)
     Rdiv(instantiate(L.A), instantiate(L.B))
 end
 
-_ldiv!(A, B) = ldiv!(factorize(A), B)
-_ldiv!(A::Factorization, B) = ldiv!(A, B)
+@inline _ldiv!(A, B) = ldiv!(factorize(A), B)
+@inline _ldiv!(A::Factorization, B) = ldiv!(A, B)
 
-_ldiv!(dest, A, B) = ldiv!(dest, factorize(A), B)
-_ldiv!(dest, A::Factorization, B) = ldiv!(dest, A, B)
-_ldiv!(dest, A::Transpose{<:Any,<:Factorization}, B) = ldiv!(dest, A, B)
-_ldiv!(dest, A::Adjoint{<:Any,<:Factorization}, B) = ldiv!(dest, A, B)
+@inline _ldiv!(dest, A, B) = ldiv!(dest, factorize(A), B)
+@inline _ldiv!(dest, A::Factorization, B) = ldiv!(dest, A, B)
+@inline _ldiv!(dest, A::Transpose{<:Any,<:Factorization}, B) = ldiv!(dest, A, B)
+@inline _ldiv!(dest, A::Adjoint{<:Any,<:Factorization}, B) = ldiv!(dest, A, B)
 
 
 
-materialize!(M::Ldiv) = _ldiv!(M.A, M.B)
-materialize!(M::Rdiv) = materialize!(Lmul(M.B', M.A'))'
-copyto!(dest::AbstractArray, M::Rdiv) = copyto!(dest', Ldiv(M.B', M.A'))'
+@inline materialize!(M::Ldiv) = _ldiv!(M.A, M.B)
+@inline materialize!(M::Rdiv) = materialize!(Lmul(M.B', M.A'))'
+@inline copyto!(dest::AbstractArray, M::Rdiv) = copyto!(dest', Ldiv(M.B', M.A'))'
 
 if VERSION ≥ v"1.1-pre"
-    copyto!(dest::AbstractArray, M::Ldiv) = _ldiv!(dest, M.A, M.B)
+    @inline copyto!(dest::AbstractArray, M::Ldiv) = _ldiv!(dest, M.A, M.B)
 else
-    copyto!(dest::AbstractArray, M::Ldiv) = _ldiv!(dest, M.A, copy(M.B))
+    @inline copyto!(dest::AbstractArray, M::Ldiv) = _ldiv!(dest, M.A, copy(M.B))
 end
 
 const MatLdivVec{styleA, styleB, T, V} = Ldiv{styleA, styleB, <:AbstractMatrix{T}, <:AbstractVector{V}}
