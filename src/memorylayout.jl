a7ebfbc87cf943454424c021bde1ca1b133ca091
Klaus Crusius <klaus.crusius@web.de>
2020-07-13 20:41:23 +0200

    replaced `data` with `parent`

--- src/memorylayout.jl
+++ src/memorylayout.jl
@@ -606,14 +606,11 @@ rowsupport(::BidiagonalLayout, A, j) =
 colsupport(::AbstractTridiagonalLayout, A, j) = max(minimum(j)-1,1):min(size(A,1),maximum(j)+1)
 rowsupport(::AbstractTridiagonalLayout, A, j) = max(minimum(j)-1,1):min(size(A,2),maximum(j)+1)
 
-data(A::Symmetric) = symmetricdata(A)
-data(A::Hermitian) = hermitiandata(A)
-
 function colsupport(::Union{SymmetricLayout,HermitianLayout}, A, j)
     if symmetricuplo(A) == 'U'
-        first(colsupport(data(A),j)):last(rowsupport(data(A),j))
+        first(colsupport(parent(A),j)):last(rowsupport(parent(A),j))
     else
-        first(rowsupport(data(A),j)):last(colsupport(data(A),j))
+        first(rowsupport(parent(A),j)):last(colsupport(parent(A),j))
     end
 end
 rowsupport(::Union{SymmetricLayout,HermitianLayout}, A, j) = colsupport(A, j)
