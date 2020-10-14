8a8297c69b587ad116c55a14b298af3b67e910f3
Sheehan Olver <solver@mac.com>
2020-10-14 14:26:01 +0100

    A[1,:] and A[1,2:end] layout_getindex

--- src/ArrayLayouts.jl
+++ src/ArrayLayouts.jl
@@ -117,6 +117,8 @@ macro _layoutgetindex(Typ)
         @inline Base.getindex(A::$Typ, kr::Colon, jr::AbstractVector) = ArrayLayouts.layout_getindex(A, kr, jr)
         @inline Base.getindex(A::$Typ, kr::Colon, jr::Integer) = ArrayLayouts.layout_getindex(A, kr, jr)
         @inline Base.getindex(A::$Typ, kr::AbstractVector, jr::Colon) = ArrayLayouts.layout_getindex(A, kr, jr)
+        @inline Base.getindex(A::$Typ, kr::Integer, jr::Colon) = ArrayLayouts.layout_getindex(A, kr, jr)
+        @inline Base.getindex(A::$Typ, kr::Integer, jr::AbstractVector) = ArrayLayouts.layout_getindex(A, kr, jr)
     end)
 end
 
