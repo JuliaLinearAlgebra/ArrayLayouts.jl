a1876b9535d0cbb674110aef83a6b96d239ceb0e
Sheehan Olver <solver@mac.com>
2020-11-13 16:49:26 +0000

    layout_getindex now works with Fill

--- src/memorylayout.jl
+++ src/memorylayout.jl
@@ -530,7 +530,13 @@ sublayout(L::AbstractFillLayout, inds::Type) = L
 reshapedlayout(L::AbstractFillLayout, _) = L
 adjointlayout(::Type, L::AbstractFillLayout) = L
 transposelayout(L::AbstractFillLayout) = L
+# TODO: Move to FillArrays.jl
+_getindex_value(V::SubArray) = getindex_value(parent(V))
+_getindex_value(V) = getindex_value(V)
 
+sub_materialize(::AbstractFillLayout, V, ax) = Fill(_getindex_value(V), ax)
+sub_materialize(::ZerosLayout, V, ax) = Zeros(ax)
+sub_materialize(::OnesLayout, V, ax) = Ones(ax)
 
 abstract type AbstractBandedLayout <: MemoryLayout end
 abstract type AbstractTridiagonalLayout <: AbstractBandedLayout end
