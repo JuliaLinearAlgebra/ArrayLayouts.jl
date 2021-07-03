9b45f65110f614d4ac6392ceff6730fc3285d271
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-07-03 10:26:33 +0200

    Don't use BandedMatrices in tests

--- test/test_layoutarray.jl
+++ test/test_layoutarray.jl
@@ -1,6 +1,5 @@
 using ArrayLayouts, LinearAlgebra, FillArrays, Base64, Test
 import ArrayLayouts: sub_materialize
-using BandedMatrices: BandedMatrix
 
 if VERSION < v"1.7-"
     ColumnNorm() = Val(true)
@@ -100,11 +99,8 @@ MemoryLayout(::Type{MyVector}) = DenseColumnMajor()
             @test inv(A) ≈ inv(A.A)
 
             S = Symmetric(MyMatrix(reshape(inv.(1:25),5,5) + 10I))
-            @test cholesky(S).U ≈ cholesky!(deepcopy(S)).U
+            @test cholesky(S).U ≈ @inferred(cholesky!(deepcopy(S))).U
             @test cholesky(S,Val(true)).U ≈ cholesky(Matrix(S),Val(true)).U
-
-            B = Symmetric(BandedMatrix(reshape(inv.(1:25),5,5) + 10I))
-            @test @inferred(cholesky(B)).U ≈ @inferred(cholesky!(deepcopy(B))).U
         end
         Bin = randn(5,5)
         B = MyMatrix(copy(Bin))
