ed1a0b8afeec94fed05968d5ce3bb243bbafcab4
Rik Huijzer <rikhuijzer@pm.me>
2022-03-19 08:55:36 +0100

    Fix typo (fix broken test)

--- test/test_muladd.jl
+++ test/test_muladd.jl
@@ -658,7 +658,7 @@ Random.seed!(0)
         a = randn(5)
         b = randn(5)
         @test ArrayLayouts.dot(a,b) == mul(a',b)
-        @test ArrayLayout.dot(a,b) ≈ dot(a,b)
+        @test ArrayLayouts.dot(a,b) ≈ dot(a,b)
         @test eltype(Dot(a,1:5)) == Float64
     end
 
