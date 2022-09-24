64d9730b2f3f2bb7d76930f7d4b4c8e87e9ab423
Daniel Karrasch <daniel.karrasch@posteo.de>
2022-09-24 13:51:06 +0200

    Update mul.jl

--- src/mul.jl
+++ src/mul.jl
@@ -196,7 +196,7 @@ macro layoutmul(Typ)
 
         Base.:*(A::LinearAlgebra.AbstractQ, B::$Typ) = ArrayLayouts.mul(A,B)
         Base.:*(A::$Typ, B::LinearAlgebra.AbstractQ) = ArrayLayouts.mul(A,B)
-        if VERSION >= v"1.9-"
+        if isdefined(LinearAlgebra, AdjointQ)
             Base.:*(A::LinearAlgebra.AdjointQ, B::$Typ) = ArrayLayouts.mul(A,B)
             Base.:*(A::$Typ, B::LinearAlgebra.AdjointQ) = ArrayLayouts.mul(A,B)
         end            
