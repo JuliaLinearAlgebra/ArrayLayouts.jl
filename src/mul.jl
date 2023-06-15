b56695a1363048dfcad560fd4f1f422a5ba8663c
Sheehan Olver <solver@mac.com>
2023-06-15 14:22:51 +0200

    Add * overloads coming from v0.9

--- src/mul.jl
+++ src/mul.jl
@@ -300,6 +300,13 @@ end
 *(A::Transpose{<:Any,<:LayoutVector}, B::Adjoint{<:Any,<:LayoutMatrix}) = mul(A,B)
 *(A::Transpose{<:Any,<:LayoutVector}, B::Transpose{<:Any,<:LayoutMatrix}) = mul(A,B)
 
+## special routines introduced in v0.9. We need to avoid these to support ∞-arrays
+
+*(x::Adjoint{<:Any,<:LayoutVector},   D::Diagonal{<:Any,<:LayoutVector}) = mul(x, D)
+*(x::AdjointAbsVec,   D::Diagonal, y::LayoutVector) = x * mul(D,y)
+*(x::TransposeAbsVec, D::Diagonal, y::LayoutVector) = x * mul(D,y)
+
+
 
 ###
 # Dot
