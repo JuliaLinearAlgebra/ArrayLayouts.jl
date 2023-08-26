aaae2db55c8c32ee865a292292719b31e61cd76b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-08-26 15:03:57 +0400

    Update docs make.jl and add CI script

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,12 @@ using ArrayLayouts
 
 makedocs(
     sitename = "ArrayLayouts",
-    format = Documenter.HTML(),
     modules = [ArrayLayouts]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/JuliaLinearAlgebra/ArrayLayouts.jl.git",
+)
