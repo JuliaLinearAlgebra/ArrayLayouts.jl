40dc4f305afca68c9b3f813d8196141512d9e1e2
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-08-20 13:37:04 +0530

    Disambiguate FillMatrix * LayoutVector (#245)
    * Disambiguate FillMatrix * LayoutVector
    
    * Restore behavior
    
    * Return FillArray
    
    * Use invoke function instead of macro
    
    * transpose and adjoint vector

--- src/mul.jl
+++ src/mul.jl
@@ -347,6 +347,11 @@ end
 *(A::Transpose{<:Any,<:LayoutVector}, B::Adjoint{<:Any,<:LayoutMatrix}) = mul(A,B)
 *(A::Transpose{<:Any,<:LayoutVector}, B::Transpose{<:Any,<:LayoutMatrix}) = mul(A,B)
 
+# Disambiguation with FillArrays
+*(A::AbstractFill{<:Any,2}, B::LayoutVector) = invoke(*, Tuple{AbstractFill{<:Any,2}, AbstractVector}, A, B)
+*(A::Adjoint{<:Any, <:LayoutVector}, B::AbstractFill{<:Any,2}) = invoke(*, Tuple{Adjoint{<:Any, <:AbstractVector}, AbstractFill{<:Any,2}}, A, B)
+*(A::Transpose{<:Any, <:LayoutVector}, B::AbstractFill{<:Any,2}) = invoke(*, Tuple{Transpose{<:Any, <:AbstractVector}, AbstractFill{<:Any,2}}, A, B)
+
 ## special routines introduced in v0.9. We need to avoid these to support ∞-arrays
 
 *(x::Adjoint{<:Any,<:LayoutVector},   D::Diagonal{<:Any,<:LayoutVector}) = mul(x, D)
