0e6a009c1ba79a82d8c3f9a26df33cce63e3dfbe
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-10-16 19:16:59 +0530

    Bump version to v1.10.2

--- test/test_layoutarray.jl
+++ test/test_layoutarray.jl
@@ -147,16 +147,7 @@ Base.copy(A::MyVector) = MyVector(copy(A.A))
 
             @testset "ldiv!" begin
                 c = MyVector(randn(5))
-                # if VERSION < v"1.9"
-                #     @test_broken ldiv!(lu(A), MyVector(copy(c))) ≈ A \ c
-                # else
                 @test ldiv!(lu(A), MyVector(copy(c))) ≈ A \ c
-                # end
-                # if VERSION < v"1.9" || VERSION >= v"1.10-"
-                #     @test_throws ErrorException ldiv!(qr(A), MyVector(copy(c)))
-                # else
-                #     @test_throws MethodError ldiv!(qr(A), MyVector(copy(c)))
-                # end
                 @test_throws ErrorException ldiv!(eigen(randn(5,5)), c)
                 @test ArrayLayouts.ldiv!(svd(A.A), Vector(c)) ≈ ArrayLayouts.ldiv!(similar(c), svd(A.A), c) ≈ A \ c
                 if VERSION ≥ v"1.8"
